76bd76a3731f4e0ce84b14d61659a35398d58cc7
Jerry Groom <Jerry.groom@gmail.com>
2013-11-11 05:18:29 -0600

    New Admin Page
    Working on a database type of Administrator page

---
+++ AdminSetup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>William Penn Bowling</title>
+        <link href="./css/frontpage.css" type="text/css" rel="stylesheet" />
+    </head>
+    <body id='pagecolor'>
+    <center><h1 id="pagename"> Administrator Login Credentials </h1></center>
+        <div class="instruction" <br />
+            <input type="text" size="30" maxlength="48" /> First Name <br />
+            <input type="text" size="30" maxlength="48" /> Last Name <br />
+            <input type="text" size="30" maxlength="48" /> Street Address <br />
+            <input type="text" size="30" maxlength="48" /> City <br />
+            <input type="text" size="30" maxlength="48" /> Zip Code <br />
+            <input type="text" size="30" maxlength="48" /> Email Address <br />
+            <br />
+            <br />
+            <input type="text" size="30" maxlength="48" /> Desired Username <br />
+            <br />
+            <input type="submit"/>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
